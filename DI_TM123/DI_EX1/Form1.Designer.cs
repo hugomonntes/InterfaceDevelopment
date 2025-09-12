52d1f0f4c167237e8bf8da1b21af347c9692d349
hugomonntes <hmontes.dev@gmail.com>
2025-09-12 12:47:49 +0200

    Update

--- DI_TM123/DI_EX1/Form1.Designer.cs
+++ DI_TM123/DI_EX1/Form1.Designer.cs
@@ -28,13 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblOperator = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.lblIgual = new System.Windows.Forms.Label();
+            this.btnSum = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblOperator
+            // 
+            this.lblOperator.AutoSize = true;
+            this.lblOperator.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperator.Location = new System.Drawing.Point(320, 193);
+            this.lblOperator.Name = "lblOperator";
+            this.lblOperator.Size = new System.Drawing.Size(25, 25);
+            this.lblOperator.TabIndex = 2;
+            this.lblOperator.Text = "+";
+            this.lblOperator.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(237, 193);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(77, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(351, 193);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(77, 20);
+            this.textBox4.TabIndex = 4;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // lblIgual
+            // 
+            this.lblIgual.AutoSize = true;
+            this.lblIgual.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIgual.Location = new System.Drawing.Point(434, 193);
+            this.lblIgual.Name = "lblIgual";
+            this.lblIgual.Size = new System.Drawing.Size(25, 25);
+            this.lblIgual.TabIndex = 5;
+            this.lblIgual.Text = "=";
+            // 
+            // btnSum
+            // 
+            this.btnSum.Location = new System.Drawing.Point(292, 230);
+            this.btnSum.Name = "btnSum";
+            this.btnSum.Size = new System.Drawing.Size(75, 23);
+            this.btnSum.TabIndex = 6;
+            this.btnSum.Text = "Suma";
+            this.btnSum.UseVisualStyleBackColor = true;
+            this.btnSum.Click += new System.EventHandler(this.btnSum_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSum);
+            this.Controls.Add(this.lblIgual);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.lblOperator);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private System.Windows.Forms.Label lblOperator;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label lblIgual;
+        private System.Windows.Forms.Button btnSum;
     }
 }
 
--- DI_TM123/DI_EX1/Form1.cs
+++ DI_TM123/DI_EX1/Form1.cs
@@ -16,5 +16,33 @@ namespace DI_EX1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSum_Click(object sender, EventArgs e)
+        {
+            try
+            {
+            lblIgual.Text = String.Format("{0}",Int32.Parse(textBox3.Text) + Int32.Parse(textBox4.Text));
+            } catch(Exception) {}
+        }
     }
 }
