300f4bd6cc6e77698056221f6859301ee867eabc
hugomonntes <hmontes.dev@gmail.com>
2025-09-12 13:16:06 +0200

    Update

---
+++ DI_TM123/DI_EX2/Program.cs
@@ -0,0 +1,19 @@
+﻿ namespace DI_EX2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nombre: ");
+            string nombre = Console.ReadLine();
+
+            Console.Write("Edad: ");
+            int edad = Int32.Parse(Console.ReadLine());
+   
+            Console.Write("Peso: ");
+            double peso = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Nombre: {0,12}, Edad: {1,4},\n \tPeso: {2,5}", nombre, edad, peso);
+        }
+    }
+}
