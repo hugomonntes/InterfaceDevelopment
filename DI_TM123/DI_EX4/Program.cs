16a1dee1ad10a041ea7941a2c4d496db97e4bdac
hugomonntes <hmontes.dev@gmail.com>
2025-09-17 09:32:05 +0200

    Update

--- DI_TM123/DI_EX4/Program.cs
+++ DI_TM123/DI_EX4/Program.cs
@@ -10,24 +10,11 @@ namespace DI_EX4
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine(isBisiesto(4000));
-            int option;
-            Console.WriteLine("Choose option:");
-            Console.WriteLine("1. Año Bisiesto");
-            Console.WriteLine("2. Suma de rango de números");
-            Console.WriteLine("3. Todas las opciones");
-            Console.WriteLine("4. Salir");
-            option = int.Parse(Console.ReadLine());
-        }
-
         public static void showMenu(int option)
         {
             switch (option)
             {
                 case 1:
-                    Console.WriteLine("Introducir funcionalidad año bisiesto");
                     int year;
                     do
                     {
@@ -35,13 +22,26 @@ namespace DI_EX4
                         year = int.Parse(Console.ReadLine());
                     }
                     while (year < 0 || year > 1000);
+                    if (isBisiesto(year))
+                    {
+                        Console.WriteLine("El año {0} es bisiesto", year);
+                    }
+                    else
+                    {
+                        Console.WriteLine("El año {0} no es bisiesto", year);
+                    }
                     break;
                 case 2:
-                    Console.WriteLine("Introducir funcionalidad suma rangos");
+                    Console.WriteLine("Introduce un numero");
+                    int n1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Introduce un numero");
+                    int n2 = int.Parse(Console.ReadLine());
+                    Console.WriteLine(sumRange(n1, n2));
                     break;
                 case 3:
-                    Console.WriteLine("Introducir funcionalidad todas las opciones");
+                    goto case 1;
                     break;
+                    goto case 2;
                 case 4:
                     Console.WriteLine("Salir");
                     break;
@@ -50,7 +50,36 @@ namespace DI_EX4
 
         public static bool isBisiesto(int year)
         {
-            return year % 4 == 0 || (year % 100 != 0 && year % 400 == 0);
+            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+        }
+
+        public static int? sumRange(int n1, int n2)
+        {
+            if (n2 > n1)
+            {
+                return null;
+            }
+            else
+            {
+                int counterSum = 0;
+                for (int i = n1; i <= n2; i++)
+                {
+                    counterSum += i;
+                }
+                return counterSum;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int option;
+            Console.WriteLine("Choose option:");
+            Console.WriteLine("1. Año Bisiesto");
+            Console.WriteLine("2. Suma de rango de números");
+            Console.WriteLine("3. Todas las opciones");
+            Console.WriteLine("4. Salir");
+            option = int.Parse(Console.ReadLine());
+            showMenu(option);
         }
     }
 }
