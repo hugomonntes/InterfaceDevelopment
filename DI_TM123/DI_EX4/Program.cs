1aa3a91cc5865cf1edd507de92996383fb461b4d
hugomonntes <hmontes.dev@gmail.com>
2025-09-17 09:35:36 +0200

    Update

--- DI_TM123/DI_EX4/Program.cs
+++ DI_TM123/DI_EX4/Program.cs
@@ -10,44 +10,6 @@ namespace DI_EX4
 {
     internal class Program
     {
-        public static void showMenu(int option)
-        {
-            switch (option)
-            {
-                case 1:
-                    int year;
-                    do
-                    {
-                        Console.WriteLine("Introduce un año");
-                        year = int.Parse(Console.ReadLine());
-                    }
-                    while (year < 0 || year > 1000);
-                    if (isBisiesto(year))
-                    {
-                        Console.WriteLine("El año {0} es bisiesto", year);
-                    }
-                    else
-                    {
-                        Console.WriteLine("El año {0} no es bisiesto", year);
-                    }
-                    break;
-                case 2:
-                    Console.WriteLine("Introduce un numero");
-                    int n1 = int.Parse(Console.ReadLine());
-                    Console.WriteLine("Introduce un numero");
-                    int n2 = int.Parse(Console.ReadLine());
-                    Console.WriteLine(sumRange(n1, n2));
-                    break;
-                case 3:
-                    goto case 1;
-                    break;
-                    goto case 2;
-                case 4:
-                    Console.WriteLine("Salir");
-                    break;
-            }
-        }
-
         public static bool isBisiesto(int year)
         {
             return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
@@ -79,7 +41,44 @@ namespace DI_EX4
             Console.WriteLine("3. Todas las opciones");
             Console.WriteLine("4. Salir");
             option = int.Parse(Console.ReadLine());
-            showMenu(option);
+            do
+            {
+                switch (option)
+                {
+                    case 1:
+                        int year;
+                        do
+                        {
+                            Console.WriteLine("Introduce un año");
+                            year = int.Parse(Console.ReadLine());
+                        }
+                        while (year < 0 || year > 1000);
+                        if (isBisiesto(year))
+                        {
+                            Console.WriteLine("El año {0} es bisiesto", year);
+                        }
+                        else
+                        {
+                            Console.WriteLine("El año {0} no es bisiesto", year);
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Introduce un numero");
+                        int n1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Introduce un numero");
+                        int n2 = int.Parse(Console.ReadLine());
+                        Console.WriteLine(sumRange(n1, n2));
+                        break;
+                    case 3:
+                        goto case 1;
+                        break;
+                        goto case 2;
+                    case 4:
+                        Console.WriteLine("Salir");
+                        break;
+                }
+            }
+            while (option != 4);
         }
     }
 }
