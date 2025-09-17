5a5c2f51d7a986097a2a0f820cb46a201a5f4368
hugomonntes <hmontes.dev@gmail.com>
2025-09-17 11:19:29 +0200

    Update

---
+++ DI_TM123/DI_EX6/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DI_EX6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            //Realizar un pequeño programa en Windows Forms que mediante un TextBox pida un
+            //texto al usuario. Tras eso, el usuario deberá pulsar un botón y se le preguntará
+            //mediante una MessageBox con Icono de Pregunta si el usuario desea poner ese
+            //texto como título del formulario principal y si contesta que sí, cambiar dicho título
+            //(Text = titulo).El texto introducido debe repetirse en la MessageBox como parte del
+            //mensaje(concatenación).
+            InitializeComponent();
+        }
+    }
+}
