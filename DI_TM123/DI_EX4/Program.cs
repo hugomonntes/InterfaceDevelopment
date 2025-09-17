692eba86d3e8ef334901e6186a3e38e61fd3d534
hugomonntes <hmontes.dev@gmail.com>
2025-09-17 09:48:55 +0200

    Update

--- DI_TM123/DI_EX4/Program.cs
+++ DI_TM123/DI_EX4/Program.cs
@@ -19,62 +19,84 @@ namespace DI_EX4
         {
             if (n2 > n1)
             {
-                return null;
-            }
-            else
-            {
-                int counterSum = 0;
-                for (int i = n1; i <= n2; i++)
+                int counterSum = n1;
+                for (int i = counterSum; i <= n2; i++)
                 {
                     counterSum += i;
                 }
                 return counterSum;
             }
+            else
+            {
+                return null;
+            }
         }
 
         static void Main(string[] args)
         {
+
             int option;
-            Console.WriteLine("Choose option:");
-            Console.WriteLine("1. Año Bisiesto");
-            Console.WriteLine("2. Suma de rango de números");
-            Console.WriteLine("3. Todas las opciones");
-            Console.WriteLine("4. Salir");
-            option = int.Parse(Console.ReadLine());
             do
             {
+                bool flag = false;
+                Console.WriteLine("1.- Año bisiesto:");
+                Console.WriteLine("2.- Suma de rango:");
+                Console.WriteLine("3.- Ambas opciones anteriores:");
+                Console.WriteLine("4.- Salir:");
+                option = int.Parse(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
-                        int year;
-                        do
+                        Console.WriteLine("Escribe un año: ");
+                        Console.WriteLine("(Debe ser positivo y menor de 10000)");
+                        int año = int.Parse(Console.ReadLine());
+                        if (año <= 0 || año > 10000)
                         {
-                            Console.WriteLine("Introduce un año");
-                            year = int.Parse(Console.ReadLine());
-                        }
-                        while (year < 0 || year > 1000);
-                        if (isBisiesto(year))
-                        {
-                            Console.WriteLine("El año {0} es bisiesto", year);
+                            Console.WriteLine("Escriba un año valido");
+                            goto case 1;
                         }
                         else
                         {
-                            Console.WriteLine("El año {0} no es bisiesto", year);
+                            if (isBisiesto(año))
+                            {
+                                Console.WriteLine("El año {0} es bisiesto", año);
+                            }
+                            else
+                            {
+                                Console.WriteLine("El año {0} no es bisiesto", año);
+                            }
+                        }
+                        if (flag)
+                        {
+                            goto case 2;
                         }
                         break;
                     case 2:
-                        Console.WriteLine("Introduce un numero");
-                        int n1 = int.Parse(Console.ReadLine());
-                        Console.WriteLine("Introduce un numero");
-                        int n2 = int.Parse(Console.ReadLine());
-                        Console.WriteLine(sumRange(n1, n2));
+                        Console.WriteLine("Escribe un numero positivo menor que 10000: ");
+                        int num1 = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Escribe otro numero positivo menor que 10000: ");
+                        int num2 = int.Parse(Console.ReadLine());
+                        if (num1 <= 0 || num1 > 10000 && num2 <= 0 || num2 > 10000)
+                        {
+                            Console.WriteLine("Escriba un año valido");
+                            goto case 2;
+                        }
+                        else
+                        {
+                            Console.WriteLine("La suma de los numeros en {0} y {1} es: {2}", num1, num2, sumRange(num1, num2));
+                        }
                         break;
                     case 3:
+                        flag = true;
                         goto case 1;
                         break;
-                        goto case 2;
+
                     case 4:
-                        Console.WriteLine("Salir");
+                        Console.WriteLine("Gracias por usar el programa.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Elija una opcion entre 1 y 4");
                         break;
                 }
             }
