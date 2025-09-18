3fb68977bfd88cf3532f20f6adc8dbb2bd59efd6
hugomonntes <hmontes.dev@gmail.com>
2025-09-18 09:56:18 +0200

    Update

--- DI_TM123/DI_EX7/Astro.cs
+++ DI_TM123/DI_EX7/Astro.cs
@@ -8,6 +8,19 @@ namespace DI_EX7
 {
     internal class Astro
     {
+        private String nombre;
+        private double radio;
 
+        public Astro()
+        {
+            nombre = "Tierra";
+            radio = 6378;
+        }
+
+        public Astro(String nombre, double radio)
+        {
+            this.nombre = nombre;
+            this.radio = radio;
+        }
     }
 }
