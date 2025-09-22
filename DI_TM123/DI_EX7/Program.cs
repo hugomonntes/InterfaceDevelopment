3e514c18a0887bf4621d3ebd55e1be9f4c99b6e9
hugomonntes <hmontes.dev@gmail.com>
2025-09-22 12:54:58 +0200

    Update

--- DI_TM123/DI_EX7/Program.cs
+++ DI_TM123/DI_EX7/Program.cs
@@ -24,7 +24,34 @@ namespace DI_EX7
             //• Salir.Como siempre no sale de la aplicación hasta que se selecciona esta
             //opción.
             List<Astro> coleccionAstros = new List<Astro>();
-            Console.WriteLine("Añadir Planeta: ");
+            Console.WriteLine("1. Añadir Planeta");
+            Console.WriteLine("2. Añade Cometa");
+            Console.WriteLine("3. Mostrar Datos");
+            Console.WriteLine("4. Incrementa / Decrementa");
+            Console.WriteLine("5. Eliminar no terraformables");
+            Console.WriteLine("6. Salir");
+            int opcion = int.Parse(Console.ReadLine());
+            bool isGaseoso;
+            do
+            {
+                switch (opcion)
+                {
+                    //• Añade Planeta: Pregunta si es gaseoso y pide el nombre y radio. También
+                    //pregunta cantidad de lunas.
+                    case 1:
+                        Astro astroToAdd = new Astro();
+                        Console.WriteLine("Es gaseoso? S/N");
+                        isGaseoso = Console.ReadLine().ToUpper() == "S" ? true : false;
+                        Console.WriteLine("Introduce el nombre: ");
+                        astroToAdd.Nombre = Console.ReadLine();
+                        Console.WriteLine("Introduce el radio: ");
+                        astroToAdd.Radio = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Introduce cantidad lunas: ");
+                        int numLunas = int.Parse(Console.ReadLine());
+                        break;
+                }
+            }
+            while (opcion != 4);
         }
     }
 }
