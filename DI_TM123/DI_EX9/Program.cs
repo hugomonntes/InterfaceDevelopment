b8631f2a08306cbe921898383da921383e27e432
hmontes <hmontes.dev@gmail.com>
2025-09-29 12:53:00 +0200

    Update

--- DI_TM123/DI_EX9/Program.cs
+++ DI_TM123/DI_EX9/Program.cs
@@ -26,13 +26,48 @@ namespace DI_EX9
             //Al pedir datos se debe comprobar que la IP es válida y que la cantidad de RAM es
             //un entero positivo.
 
-            int opcion;
-            Console.WriteLine("1- ");
-            switch (opcion)
+            int option = 0;
+            do
             {
-                case 1:
-                    break;
+                Console.WriteLine("1.- Añadir nuevo equipo");
+                Console.WriteLine("2.- Añadir varios equipos");
+                Console.WriteLine("3.- Elimina un dato por clave");
+                Console.WriteLine("4.- Mostrar coleccion completa(solo ips)");
+                Console.WriteLine("5.- Muestra un elemento de la coleccion (mostrar RAM o que no existe)");
+                Console.WriteLine("6.- Salir");
+                option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+
+                        break;
+
+                    case 2:
+
+                        break;
+
+                    case 3:
+
+                        break;
+
+                    case 4:
+
+                        break;
+
+                    case 5:
+
+                        break;
+
+                    case 6:
+
+                        break;
+
+                    default:
+                        Console.WriteLine("Elija una opcion del menu");
+                        break;
+                }
             }
+            while (option != 6);
 
         }
     }
