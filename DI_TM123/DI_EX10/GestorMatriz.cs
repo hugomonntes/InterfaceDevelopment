1d317f16fbc64fcf8c7871bc64e18a4cc102c5e6
hmontes <hmontes.dev@gmail.com>
2025-10-02 12:13:15 +0200

    Update

---
+++ DI_TM123/DI_EX10/GestorMatriz.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DI_EX10
+{
+    internal class GestorMatriz
+    {
+        public int[,] Matriz;
+
+        public GestorMatriz(int numeroFilas, int numeroColumnas)
+        {
+            Matriz = new int[randomNumber(-5,20), randomNumber(-5, 20)];
+        }
+
+        public int randomNumber(int minRangue, int maxRangue)
+        {
+            Random random = new Random();
+            return random.Next(minRangue, maxRangue);
+        }
+    }
+}
