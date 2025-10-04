f5fe79c2b0930baab1d0121e65a68433eac369a4
hugomonntes <huugomontees@gmail.com>
2025-10-04 11:47:04 +0200

    Update

--- DI_TM123/DI_EX10/GestorMatriz.cs
+++ DI_TM123/DI_EX10/GestorMatriz.cs
@@ -9,18 +9,38 @@ namespace DI_EX10
     internal class GestorMatriz
     {
         public int[,] Matriz;
-
+        private static Random random = new Random();
         public GestorMatriz(int numeroFilas, int numeroColumnas)
         {
-            Matriz = new int[randomNumber(-5,20), randomNumber(-5, 20)];
+            Matriz = new int[numeroFilas, numeroColumnas];
+            for (int i = 0; i < numeroFilas; i++)
+            {
+                for (int j = 0; j < numeroColumnas; j++)
+                {
+                    Matriz[i, j] = random.Next(-5, 21);
+                }
+            }
         }
 
         public GestorMatriz() : this(3, 4) { }
 
-        //public (int, bool) sumaFila(int numeroFilas)
-        //{
-        //    return numeroFilas > 0;
-        //}
+        //El booleano devuelve true si el parámetro es un índice válido
+        //y false si no lo es.En caso de índice válido el entero devuelto es la suma de los
+        //elementos de la fila. Si no devuelve 0.
+        public (int, bool) sumaFila(int indiceFila)
+        {
+            if (indiceFila < 0 || indiceFila >= Matriz.GetLength(0))
+            {
+                return (0, false);
+            }
+            int suma = 0;
+            for (int j = 0; j < Matriz.GetLength(1); j++)
+            {
+                suma += Matriz[indiceFila, j];
+            }
+
+            return (suma, true);
+        }
 
         public int randomNumber(int minRangue, int maxRangue)
         {
