6913c089c0e834a2c429a7a0cd6a5be336f8ba85
hugomonntes <huugomontees@gmail.com>
2025-11-15 13:17:37 +0100

    feat(exam): add exam forms, checkboxes

--- DI_TM123/DI_EX_1EVAL/FrmPrincipal.Designer.cs
+++ DI_TM123/DI_EX_1EVAL/FrmPrincipal.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnJugar = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btnJugar
+            // 
+            this.btnJugar.Location = new System.Drawing.Point(713, 12);
+            this.btnJugar.Name = "btnJugar";
+            this.btnJugar.Size = new System.Drawing.Size(75, 23);
+            this.btnJugar.TabIndex = 0;
+            this.btnJugar.Text = "Jugar";
+            this.btnJugar.UseVisualStyleBackColor = true;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(713, 41);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 1;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(713, 70);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 2;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
             // 
             // FrmPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnJugar);
             this.Name = "FrmPrincipal";
             this.Text = "Loteria SImple";
             this.ResumeLayout(false);
@@ -42,6 +75,10 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnJugar;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnSalir;
     }
 }
 
--- DI_TM123/DI_EX_1EVAL/FrmPrincipal.cs
+++ DI_TM123/DI_EX_1EVAL/FrmPrincipal.cs
@@ -39,10 +39,39 @@ namespace DI_EX_1EVAL
                 this.Controls.Add(newCb);
             }
         }
+
+        public void animacionColorBotones(Button[] btns)
+        {
+            Timer tm = new Timer();
+            tm.Interval = 300;
+            tm.Start();
+
+            int i = 0;
+            tm.Tick += (sender, e) =>
+            {
+                if (btns[i].BackColor == Color.Beige)
+                {
+                    btns[i].BackColor = Color.Yellow;
+                }
+                else
+                {
+                    btns[i].BackColor = Color.Beige;
+                }
+
+
+                i++;
+                if (i >= btns.Length)
+                {
+                    i = 0;
+                }
+            };
+        }
         public FrmPrincipal()
         {
             InitializeComponent();
             initCheckBoxes(49);
+            Button[] btns = { btnJugar, btnReset, btnSalir};
+            animacionColorBotones(btns);
         }
     }
 }
