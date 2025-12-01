64f2f024782638090425ee49a3641f2abcfd8537
hmontes <hmontes.dev@gmail.com>
2025-12-01 13:25:02 +0100

    Update

---
+++ DI_TM123/NuevosProgramas/LabelTextBox.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NuevosProgramas
+{
+    public enum EPosicion
+    {
+        IZQUIERDA, DERECHA
+    }
+    public partial class LabelTextBox : UserControl
+    {
+        private EPosicion posicion = EPosicion.IZQUIERDA;
+        [Category("Nuevas")]
+        [Description("Indica si la Label se sitúa a la IZQUIERDA o DERECHA del Textbox")]
+        public EPosicion Posicion
+        {
+            set
+            {
+                if (Enum.IsDefined(typeof(EPosicion), value))
+                {
+                    posicion = value;
+                    recolocar();
+                }
+                else
+                {
+                    throw new InvalidEnumArgumentException();
+                }
+            }
+            get
+            {
+                return posicion;
+            }
+        }
+
+        //Pixeles de separación entre label y textbox
+        private int separacion = 0;
+        [Category("Nuevas")] // O se puede meter en categoría Design.
+        [Description("Píxels de separación entre Label y Textbox")]
+        public int Separacion
+        {
+            set
+            {
+                if (value >= 0)
+                {
+                    separacion = value;
+                    recolocar();
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+            get
+            {
+                return separacion;
+            }
+        }
+
+        [Category("Nuevas")] // O se puede meter en categoría Appearance.
+        [Description("Texto asociado a la Label del control")]
+        public string TextLbl
+        {
+            set
+            {
+                lbl.Text = value;
+            }
+            get
+            {
+                return lbl.Text;
+
+            }
+        }
+
+        [Category("Nuevas")]
+        [Description("Texto asociado al TextBox del control")]
+        public string TextTxt
+        {
+            set
+            {
+                txt.Text = value;
+            }
+            get
+            {
+                return txt.Text;
+            }
+        }
+
+        void recolocar()
+        {
+            switch (posicion)
+            {
+                case EPosicion.IZQUIERDA:
+                    //Establecemos posición del componente lbl
+                    lbl.Location = new Point(0, 0);
+                    // Establecemos posición componente txt
+                    txt.Location = new Point(lbl.Width + Separacion, 0);
+                    //Establecemos ancho del Textbox
+                    //(la label tiene ancho por autosize)
+                    txt.Width = this.Width - lbl.Width - Separacion;
+                    //Establecemos altura del componente
+                    this.Height = Math.Max(txt.Height, lbl.Height);
+                    break;
+                case EPosicion.DERECHA:
+                    //Establecemos posición del componente txt
+                    txt.Location = new Point(0, 0);
+                    //Establecemos ancho del Textbox
+                    txt.Width = this.Width - lbl.Width - Separacion;
+                    //Establecemos posición del componente lbl
+                    lbl.Location = new Point(txt.Width + Separacion, 0);
+                    //Establecemos altura del componente (Puede sacarse del switch)
+                    this.Height = Math.Max(txt.Height, lbl.Height);
+                    break;
+            }
+        }
+
+        public LabelTextBox()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            recolocar();
+        }
+    }
+}
